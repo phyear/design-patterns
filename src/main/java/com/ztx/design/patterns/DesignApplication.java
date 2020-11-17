ef9481dd9e39fe78c243da3d833303d1258fe258
zhaotianxin <ztxemail@163.com>
2020-11-17 15:56:08 +0800

    [IMP] 初始化配置项目

---
+++ src/main/java/com/ztx/design/patterns/DesignApplication.java
@@ -0,0 +1,13 @@
+package com.ztx.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignApplication.class, args);
+    }
+
+}
